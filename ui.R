ac01bea35058cd87ba0b417f1c1b2357c4b25add
J.Bowman <bolight@gmail.com>
2015-05-21 11:39:52 +0000

    add help text to ui.R

--- ui.R
+++ ui.R
@@ -1,20 +1,25 @@
 shinyUI(
   pageWithSidebar(
     # Application title
-    headerPanel("Pythagorean Win calculator"),
+    headerPanel("Pythagorean Win Calculator"),
     sidebarPanel(
       selectInput('team', 'Select a team',
                   choices = unique(Teams$name[order(Teams$name)]),
                   selected = 'Detroit Tigers'),
-      numericInput('year', 'Select a year',
+      numericInput('year', 'Select a season',
                    value = 1984, min = 1901, max = 2013, step = 1),
-      actionButton('yrAction', 'Calculate!')
+      actionButton('yrAction', 'Calculate!'),
+      withMathJax(
+        helpText(HTML("Calculates <a href=\"https://en.wikipedia.org/wiki/Pythagorean_expectation\">pythagorean (expected) wins</a>"),
+                 "for the selected baseball team and season. This is calculated using the formula",
+                 "$$\\frac{(Runs)^a}{(Runs)^a + (Runs Allowed)^a}$$",
+                 "where the exponent \\(a\\) is calculated using the",
+                 HTML("'<a href=\"https://en.wikipedia.org/wiki/Pythagenpat\">Pythagenpat</a>' method."),
+               "If you select a season where the selected team did not play, the",
+               "selection box will be reset to the nearest season they did play."))
       ),
     mainPanel(
        h3('Results'),
-#       h4('You entered'),
-#       verbatimTextOutput("team"),
-#       h4('Which resulted in a prediction of '),
        uiOutput("result")
     )
   )
