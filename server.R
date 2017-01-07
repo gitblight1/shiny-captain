2673094267ee5b73ea4bc7c35f1dc6a1c042b98f
J.Bowman <bolight@gmail.com>
2017-01-07 17:37:20 -0500

    code reformatting

--- server.R
+++ server.R
@@ -2,8 +2,7 @@ library(Lahman) # gotta load this here and in ui.R
 
 # Calculate expected win percentage
 pythagWpct <- function(rScored, rAllowed, exponent) {
-  
-  rScored^exponent / (rScored^exponent + rAllowed^exponent)
+    rScored ^ exponent / (rScored ^ exponent + rAllowed ^ exponent)
 }
 
 origExp <- 2
@@ -12,76 +11,81 @@ brExp <- 1.83
 
 portExp <- function(rScored, rAllowed, games) {
     # Calculate the exponent using the 'Pythagenport' method
-    exponent = 1.5 + log((rScored + rAllowed)/games) + 0.45
+    exponent = 1.5 + log((rScored + rAllowed) / games) + 0.45
 }
 
 patExp <- function(rScored, rAllowed, games) {
     # Calculate the exponent using the 'Pythagenpat' method
-    exponent = ((rScored + rAllowed)/games)^0.287
+    exponent = ((rScored + rAllowed) / games) ^ 0.287
 }
 
 makeTable <- function(season) {
-    rows = c('Original', 'Baseball Reference', 'Pythagenport', 'Pythagenpat')
-    expList = c(origExp, brExp, with(season, portExp(R, RA, G)), with(season, patExp(R, RA, G)))
+    rows = c('Original',
+             'Baseball Reference',
+             'Pythagenport',
+             'Pythagenpat')
+    expList = c(origExp,
+                brExp,
+                with(season, portExp(R, RA, G)),
+                with(season, patExp(R, RA, G)))
     pWpct = with(season, pythagWpct(R, RA, expList))
-    pythagRecord <- data.frame('pct.' = round(pWpct,4), 'Wins' = with(season, as.integer(round(G*pWpct, 0))), 'Losses' = with(season, G - as.integer(round(G*pWpct, 0))), row.names = rows)
+    pythagRecord <-
+        data.frame(
+            'pct.' = round(pWpct, 4),
+            'Wins' = with(season, as.integer(round(G * pWpct, 0))),
+            'Losses' = with(season, G - as.integer(round(G * pWpct, 0))),
+            row.names = rows
+        )
 }
 
 # Format the output string and return it as an HTML element.
 formatResult <- function(season) {
-
-  result = with(
-      season,
-      sprintf(
-          "In %d, the %s scored %d runs and allowed %d in %d games.<br/>
-          Their actual record was %d-%d.<br/>
-          Expected win percentage(s) and record(s):",
-          yearID,
-          name,
-          R,
-          RA,
-          G,
-          W,
-          L
-      )
-  )
-  HTML(result)
+    result = with(
+        season,
+        sprintf(
+            "In %d, the %s scored %d runs and allowed %d in %d games.<br/>
+            Their actual record was %d-%d.<br/>
+            Expected win percentage(s) and record(s):",
+            yearID,
+            name,
+            R,
+            RA,
+            G,
+            W,
+            L
+        )
+    )
+    HTML(result)
 }
 
-shinyServer(
-  function(input, output, session) {
-
-    yrRng = reactive(
-      # When a team is selected, get the years they played
-      sort(Teams$yearID[Teams$name == input$team])
-    )
+shinyServer(function(input, output, session) {
+    yrRng = reactive(# When a team is selected, get the years they played
+        sort(Teams$yearID[Teams$name == input$team]))
     observe({
-      # Update the valid year inputs based on yrRange. This only updates when
-      # the Calculate button is pressed, and should disallow years when the selected
-      # franchise did not play.
-      input$yrAction
-      isolate({
-      yrs = yrRng()
-      # if the current year is outside the range, reset it to the nearest year in the range.
-      curYr = yrs[which.min(abs(yrs-as.numeric(input$year)))]
-      updateSelectInput(session, 'year',
-                         choices = yrs,
-                         selected = curYr)
-      })
+        # Update the valid year inputs based on yrRange. This only updates when
+        # the Calculate button is pressed, and should disallow years when the selected
+        # franchise did not play.
+        input$yrAction
+        isolate({
+            yrs = yrRng()
+            # if the current year is outside the range, reset it to the nearest year in the range.
+            curYr = yrs[which.min(abs(yrs - as.numeric(input$year)))]
+            updateSelectInput(session,
+                              'year',
+                              choices = yrs,
+                              selected = curYr)
+        })
     })
-    season <- reactive(
-      # grab info about the season when a team and year are selected.
-      Teams[Teams$name == input$team & Teams$yearID == input$year,]
-    )
-
+    season <- reactive(# grab info about the season when a team and year are selected.
+        Teams[Teams$name == input$team & Teams$yearID == input$year, ])
+    
     # create the main result when the calculate button is clicked.
     output$result <- renderUI({
-      input$yrAction
-      isolate(formatResult(season()))
+        input$yrAction
+        isolate(formatResult(season()))
     })
     output$data <- renderTable({
         input$exponent
-        isolate(makeTable(season())[input$exponent,,drop=FALSE])
+        isolate(makeTable(season())[input$exponent, , drop = FALSE])
     }, rownames = TRUE)
-  }
-)
+})
--- ui.R
+++ ui.R
@@ -1,41 +1,53 @@
 library(Lahman) # gotta include this here and in server.R
 
-shinyUI(
-  pageWithSidebar(
+shinyUI(pageWithSidebar(
     # Application title
     headerPanel("Pythagorean Win Calculator"),
     # Input section
     sidebarPanel(
-      selectInput('team', 'Select a team',
-                  choices = unique(Teams$name[order(Teams$name)]),
-                  selected = 'Detroit Tigers'),
-      selectInput('year', 'Select a season',
-                   selected = 1984, choices = seq(1901,2015)),
-      #TODO add checkboxes for exponents here
-      checkboxGroupInput('exponent', 'Select calculation methods:',
-                         c("Original",
-                           "Baseball Reference",
-                           "Pythagenport",
-                           "Pythagenpat")
-                        ),
-      actionButton('yrAction', 'Calculate!'),
-      
-      withMathJax(
-        helpText(HTML("Calculates <a href=\"https://en.wikipedia.org/wiki/Pythagorean_expectation\">pythagorean (expected) wins</a>"),
-                 "for the selected baseball team and season (data exists through the 2013 season).",
-                 "This is calculated using the formula",
-                 "$$\\frac{(Runs)^a}{(Runs)^a + (Runs Allowed)^a}$$",
-                 "where the exponent \\(a\\) is calculated up to four different ways.",
-                 "Use the checkboxes to select the method(s) to calculate the exponent;",
-                 "each method is explained in the link above.",
-               "If you select a season where the selected team did not play, the",
-               "selection box will be reset to the nearest season they did play."))
-      ),
+        selectInput(
+            'team',
+            'Select a team',
+            choices = unique(Teams$name[order(Teams$name)]),
+            selected = 'Detroit Tigers'
+        ),
+        selectInput(
+            'year',
+            'Select a season',
+            selected = 1984,
+            choices = seq(1901, 2015)
+        ),
+        #TODO add checkboxes for exponents here
+        checkboxGroupInput(
+            'exponent',
+            'Select calculation methods:',
+            c(
+                "Original",
+                "Baseball Reference",
+                "Pythagenport",
+                "Pythagenpat"
+            )
+        ),
+        actionButton('yrAction', 'Calculate!'),
+        
+        withMathJax(
+            helpText(
+                HTML(
+                    "Calculates <a href=\"https://en.wikipedia.org/wiki/Pythagorean_expectation\">pythagorean (expected) wins</a>"
+                ),
+                "for the selected baseball team and season (data exists through the 2013 season).",
+                "This is calculated using the formula",
+                "$$\\frac{(Runs)^a}{(Runs)^a + (Runs Allowed)^a}$$",
+                "where the exponent \\(a\\) is calculated up to four different ways.",
+                "Use the checkboxes to select the method(s) to calculate the exponent;",
+                "each method is explained in the link above.",
+                "If you select a season where the selected team did not play, the",
+                "selection box will be reset to the nearest season they did play."
+            )
+        )
+    ),
     # Output section
-    mainPanel(
-       h3('Results'),
-       uiOutput("result"), # output is dynamically rendered in server.R
-       tableOutput("data")
-    )
-  )
-)
+    mainPanel(h3('Results'),
+              uiOutput("result"), # output is dynamically rendered in server.R
+              tableOutput("data"))
+))
